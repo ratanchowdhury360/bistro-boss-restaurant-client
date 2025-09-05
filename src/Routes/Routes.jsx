a1ff63fb7bada2e8955a1328209331b4c46b60f8
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-05 21:18:13 +0600

    Use swiper JS to create a category Swiper

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,7 +3,8 @@ import {
 
 } from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home/Home";
+
 
 
  export const router = createBrowserRouter([
