23f83cea888180d802f44545c095367f002ef201
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-06 20:14:02 +0600

    create parallax

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -7,10 +7,13 @@ import featuredImg1 from '../../../assets/home/featured.jpg';
 
 const Featured = () => {
     return (
-        <div className="fatured-item my-20 text-white " style={{ backgroundImage: `url(${featuredImg1})` }} >
+        <div className="  my-20 text-white  " style={{
+             backgroundImage: `url(${featuredImg1})`,
+             backgroundAttachment: 'fixed'
+             }} >
             <SectionTitle subHeading={"Check it out"} heading={"Featured Item"}> </SectionTitle>
 
-            <div className="md:flex gap-10 items-center text-white px-36 py-20 pb-20 pt-12"  >
+            <div className="md:flex gap-10 items-center bg-slate-500 bg-opacity-60 px-36 py-20 pb-20 pt-12"  >
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -21,7 +24,7 @@ const Featured = () => {
                         nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Viverra aliquet
                         eget sit amet tellus. Ornare lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus dolor purus non.
                     </p>
-                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+                    <button className="btn btn-outline text-white border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
