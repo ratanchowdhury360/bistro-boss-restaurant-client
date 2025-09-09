bab42a095b2722b03168b7b3c4861f64161652a2
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-09 13:37:13 +0600

    simple login form

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,7 @@ import Home from "../Pages/Home/Home/Home";
 
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
 
 
 
@@ -25,6 +26,10 @@ import Order from "../Pages/Order/Order/Order";
     {
       path : 'order/:category',
       element: <Order></Order>
+    },
+    {
+      path: 'login',
+      element: <Login></Login>
     }
   ]
   },
