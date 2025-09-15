9b61deb4884f7e99923aae6be1e69d43f53e8221
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-16 03:11:19 +0600

    Fix Login issue and API race condition

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,31 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
 
-import { createContext, useEffect, useState } from "react";
+
+import {  createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
-
-
-
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
+
 const auth = getAuth(app);
 
-const updateUserProfile = (name, photo) => {
-    return updateProfile(auth.currentUser, {
-        displayName: name, photoURL: photo
-    });
-}
-
-
-const AuthProvider = ({children}) => {
-    const [user, setUser] =useState(null);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-     const googleProvider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
-
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signIn = (email, password) => {
@@ -33,8 +26,7 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
-
-    const googleSignIn = () =>{
+    const googleSignIn = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider);
     }
@@ -44,37 +36,46 @@ const AuthProvider = ({children}) => {
         return signOut(auth);
     }
 
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
+
     useEffect(() => {
-       const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            //console.log('auth state changed', currentUser);
-            if(currentUser){
+            if (currentUser) {
                 // get token and store client
-                
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                            setLoading(false);
+                        }
+                    })
             }
-            else{
+            else {
                 // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+                localStorage.removeItem('access-token');
+                setLoading(false);
             }
-            setLoading(false);
-        }
-        );
+
+        });
         return () => {
             return unsubscribe();
         }
-    }, [])
-
-
+    }, [axiosPublic])
 
     const authInfo = {
         user,
         loading,
-        createUser ,
+        createUser,
         signIn,
         googleSignIn,
         logOut,
         updateUserProfile
-
     }
 
     return (
